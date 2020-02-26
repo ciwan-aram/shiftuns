74c30ffac3680de7ca8e95621fbd37242185bd04
root <root@eCiwan.localdomain>
2020-02-27 00:56:07 +0100

    node modules excluded

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 /* We'll centralize our routes imports to this file to keep our code clean */
 
-const router = require("express").Router();
-const usersRoutes = require("./users");
-const postsRoutes = require("./posts");
+const router = require('express').Router();
+const usersRoutes = require('./users');
+const postsRoutes = require('./posts');
 
-router.get("/", (req, res) => {
-  res.send("This is home");
+router.get('/', (req, res) => {
+  res.send('Welcome to shifTuns');
 });
 
-router.use("/api", usersRoutes);
-router.use("/api", postsRoutes);
+router.use('/api', usersRoutes);
+router.use('/api', postsRoutes);
 
 module.exports = router;
